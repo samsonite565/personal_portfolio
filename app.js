42efd80df2b06365861dec24b930f4d9570f5392
Samson Thomas <samthomas553@gmail.com>
2025-04-15 23:27:06 +0530

    just made some minor adjustment to media queries

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let readTag = document.querySelector('.readmore');
-let paragraph = document.querySelector('.p2');
+let readTag = document.querySelector(".readmore");
+let paragraph = document.querySelector(".p2");
 function readMore() {
-    //Toggle the read more on and off so that it shows the paragraph
-    paragraph.classList.toggle('p2');
-    readTag.style.display='none'
-};
-readTag.addEventListener('click', readMore);
+  //Toggle the read more on and off so that it shows the paragraph
+  paragraph.classList.toggle("p2");
+  if (paragraph.classList.contains("p2")) {
+    readTag.textContent='Read more...'
+    
+  } else {
+    readTag.textContent = "Read less...";
+  };
+}
+readTag.addEventListener("click", readMore);
