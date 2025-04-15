9e8c0f8403f6787d73e890b650ffbe77fe43d98f
Samson Thomas <samthomas553@gmail.com>
2025-04-15 18:36:53 +0530

    added a hover effect, read more effect

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log('hello world')
-
+let readTag = document.querySelector('.readmore');
+let paragraph = document.querySelector('.p2');
+function readMore() {
+    //Toggle the read more on and off so that it shows the paragraph
+    paragraph.classList.toggle('p2');
+    readTag.style.display='none'
+};
+readTag.addEventListener('click', readMore);
