33d3b987fc6d86b426633a522899ac2175c057e5
Samson Thomas <samthomas553@gmail.com>
2025-04-17 18:42:26 +0530

    test the view resusme option also add the pdf

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let readTag = document.querySelector(".readmore");
 let paragraph = document.querySelector(".p2");
+let pdfViewer = document.querySelector('.overlay');
+let viewResumeBtn = document.querySelector('.aboutme_resume ')
+
+
 function readMore() {
   //Toggle the read more on and off so that it shows the paragraph
   paragraph.classList.toggle("p2");
@@ -11,3 +15,14 @@ function readMore() {
   };
 }
 readTag.addEventListener("click", readMore);
+
+function viewPdf() {
+  paragraph.classList.toggle('overlay');
+  // if (pdfViewer.contains('overlay')) {
+  //   pdfViewer.style.display = 'block';
+  // } else {
+
+  // };
+};
+
+viewResumeBtn.addEventListener('click', viewPdf)
